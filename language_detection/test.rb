b50bcc8800282c91fde4817fd77c374d24fd0f31
Yuya Unno <unno@preferred.jp>
2013-09-19 23:37:13 +0900

    Fix sort order

--- language_detection/test.rb
+++ language_detection/test.rb
@@ -15,5 +15,5 @@ while buf = Readline.readline("> ", true)
   cli = Jubatus::Classifier::Client::Classifier.new "127.0.0.1", 9199, NAME
   data = Jubatus::Common::Datum.new("text" => buf.chomp)
   result = cli.classify [data]
-  pp result[0].sort_by{|x| x.score}
+  pp result[0].sort_by{|x| -x.score}
 end
